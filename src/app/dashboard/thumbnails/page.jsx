9a39a48bf1bad9c59bc2cf1acd24da5035c24303
shravanbhati <dev.shravanbhati@gmail.com>
2025-09-09 23:51:44 +0530

    fix redirect back to login page issue

--- src/app/dashboard/thumbnails/page.jsx
+++ src/app/dashboard/thumbnails/page.jsx
@@ -2,21 +2,54 @@
 import React from "react";
 import {
   PromptInput,
+  PromptInputActionAddAttachments,
+  PromptInputActionMenu,
+  PromptInputActionMenuContent,
+  PromptInputActionMenuItem,
+  PromptInputActionMenuTrigger,
+  PromptInputAttachment,
+  PromptInputAttachments,
+  PromptInputBody,
+  PromptInputButton,
   PromptInputSubmit,
   PromptInputTextarea,
   PromptInputToolbar,
-} from "@/components/prompt-input";
+  PromptInputTools,
+  usePromptInputAttachments,
+} from "@/components/ai-elements/prompt-input";
+import { useIsMobile } from "@/hooks/use-mobile";
+import { Image } from "lucide-react";
+import { Sparkles } from "lucide-react";
 const page = () => {
+  const isMobile = useIsMobile();
+  const [prompt, setPrompt] = React.useState("");
   return (
     <div>
-      <PromptInput onSubmit={() => {}} className="mt-4 relative">
-        <PromptInputTextarea onChange={(e) => {}} value={""} />
-        <PromptInputToolbar>
-          <PromptInputSubmit
-            className="absolute right-1 bottom-1 rounded-full"
-            disabled={false}
-            status={"ready"}
+      <PromptInput
+        onSubmit={() => {}}
+        className="mt-4 relative border-0 dark:bg-zinc-800"
+      >
+        <PromptInputBody>
+          <PromptInputAttachments>
+            {(attachment) => <PromptInputAttachment data={attachment} />}
+          </PromptInputAttachments>
+          <PromptInputTextarea
+            onChange={(e) => setPrompt(e.target.value)}
+            value={prompt}
           />
+        </PromptInputBody>
+        <PromptInputToolbar>
+          <PromptInputTools>
+            <PromptInputButton size={100}>
+              <Image />
+              <span>Faces & Objects</span>
+            </PromptInputButton>
+            <PromptInputButton>
+              <Sparkles />
+              <span>Reference</span>
+            </PromptInputButton>
+          </PromptInputTools>
+          <PromptInputSubmit disabled={false} status={"ready"} />
         </PromptInputToolbar>
       </PromptInput>
     </div>
